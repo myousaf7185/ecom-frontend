0ce258299d89d5bf7d77ea2961f6b8665e2ddfbf
Muhammad Yousaf <myousaf7185@gmail.com>
2024-08-29 22:25:35 +0500

    Initial commit

---
+++ src/seller/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='text-center' style={{marginTop:"130px"}} ><h3>Welcome to Dashboard</h3></div>
+
+  )
+}
+
+export default Dashboard
